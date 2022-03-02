7e34b459e624e6e858b4bc92a267dac3adf5804b
Julian Langschaedel <julian@langschaedel.com>
2022-03-02 19:03:17 +0100

    DarknetImage move float conversion into c code
    imgTofloat32 + fill_image_f32 took more than double the time since tofloat created needless allocations and iterate over each pixel, and fill_image in c then iterate over each pixel*3 again just to move it into the c image.

--- image.h
+++ image.h
@@ -4,3 +4,4 @@
 
 extern void fill_image_f32(image *im, int w, int h, int c, float* data);
 extern void set_data_f32_val(float* data, int index, float value);
+extern void to_float_and_fill_image(image *im, int w, int h, uint8_t* data);
